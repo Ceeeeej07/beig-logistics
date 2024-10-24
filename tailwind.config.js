4d1fcd3a6a6e316f9d87e5da82a2e7a079797d23
Carl J <cjcsantoceldes07@gmail.com>
2024-10-24 21:45:32 +0800

    beig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+            },
+            backgroundColor: {
+                custom: "#f0f0f0", // Set your custom background color here
+            },
+            fontSize: {
+                custom: ["20px", "30px"], // Custom size with line height
+            },
+            textColor: {
+                "yellow-500": "#ffa500",
             },
         },
     },
